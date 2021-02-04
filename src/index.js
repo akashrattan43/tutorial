fecf1392eb2799ec2bee470bde3193928dace9b9
akash rattan <akashrattan43@Gmail.com>
2021-02-03 19:50:09 -0800

    book list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <h1>John Doe</h1>
-      <p>This is my message</p>
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => (
+  <article>
+    <Image />
+    <Title />
+    <Author />
+  </article>
+);
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL320_.jpg"
+    alt=""
+  />
+);
+
+const Title = () => <h1>Love you to the moon and back</h1>;
+
+const Author = () => <h1>Amelia Hepworth</h1>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
+ 
