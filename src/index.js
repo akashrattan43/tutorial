0a124e3fb1937b7100978a6260ca20a6d858e4ca
akash rattan <akashrattan43@Gmail.com>
2021-02-03 20:44:35 -0800

    last changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 
 function BookList() {
   return (
-    <section className='booklist'>
+    <section className="booklist">
       <Book />
       <Book />
       <Book />
@@ -13,15 +13,18 @@ function BookList() {
   );
 }
 
-const Book = () => (
-  <article className='book'>
-     <img
-    src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL320_.jpg"
-    alt=""
-  />
-    <h1>Love you to the moon and back</h1>;
-    <h1>Amelia Hepworth</h1>;
-  </article>
-);
+const Book = () => {
+  const title = "Love you to the moon and back";
+  return (
+    <article className="book">
+      <img
+        src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL320_.jpg"
+        alt=""
+      />
+      <h1>{title}</h1>
+      <h1>Amelia Hepworth</h1>
+    </article>
+  );
+};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
