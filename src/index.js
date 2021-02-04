d072f019f3c60ff58d0b58091106953a6464fa13
akash rattan <akashrattan43@Gmail.com>
2021-02-04 12:49:42 -0800

    props

--- src/index.js
+++ src/index.js
@@ -2,27 +2,43 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
+//setup vars
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL320_.jpg",
+  title: "Love you to the moon and back",
+  author: "Amelia Hepworth",
+};
+const secondBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/41qmSL688OL._SX316_BO1,204,203,200_.jpg",
+  title: "You Are a Badass",
+  author: "Jen Sincero",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
-  const title = "Love you to the moon and back";
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL320_.jpg"
-        alt=""
-      />
-      <h1>{title}</h1>
-      <h1>Amelia Hepworth</h1>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h1>{props.author}</h1>
     </article>
   );
 };
