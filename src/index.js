7330bbf919573f37f21619e544e0c996ab2326ea
akash rattan <akashrattan43@Gmail.com>
2021-02-04 21:13:16 -0800

    wo

--- src/index.js
+++ src/index.js
@@ -3,43 +3,38 @@ import ReactDom from "react-dom";
 import "./index.css";
 
 //setup vars
-const firstBook = {
+const books = [
+  {
   img: "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL320_.jpg",
   title: "Love you to the moon and back",
   author: "Amelia Hepworth",
-};
-const secondBook = {
+},
+{
   img:
     "https://images-na.ssl-images-amazon.com/images/I/41qmSL688OL._SX316_BO1,204,203,200_.jpg",
   title: "You Are a Badass",
   author: "Jen Sincero",
-};
+}
+]
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      > <p>Lorem ipsum dolar sit</p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        const {img, title, author} = book
+        return <Book book={book}></Book>
+      })}
     </section>
   );
 }
 
-const Book = ({img, title, author, children}) => {
+const Book = (props) => {
+   const {img, title, author} = props.book
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h1>{author}</h1>
-      {children}
     </article>
   );
 };
